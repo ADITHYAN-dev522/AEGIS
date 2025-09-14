5dc98b40a2c92474b96e60b5744ea815abf7e767
Adithyan <adithyanu375@gmail.com>
2025-09-14 10:41:28 +0530

    Update utils.py

--- aegis-comm/aegis-comm/src/utils.py
+++ aegis-comm/aegis-comm/src/utils.py
@@ -1,1 +1,17 @@
 # src/utils.py placeholder
+import qrcode
+from PIL import Image
+import io
+
+def generate_qr(data: str, filename: str = None):
+    """Generate QR for key exchange demo."""
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    if filename:
+        img.save(filename)
+    return img
+
+def load_image(path: str) -> Image.Image:
+    return Image.open(path)
